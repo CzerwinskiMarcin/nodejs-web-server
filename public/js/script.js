f071535969a489424382bb529f022fa5cf7aaffc
Marcin Czerwinski <marbroczerwinski@gmail.com>
2021-09-21 23:29:55 +0200

    init project

---
+++ public/js/script.js
@@ -0,0 +1,43 @@
+const form = document.getElementById('weather_form');
+const resultElement = document.getElementById('weather_result');
+const resultContainer = document.getElementById('result_container');
+
+form.onsubmit = () => {
+    return false;
+}
+console.log(form);
+
+function getData() {
+    showLoader();
+    const location = document.getElementById('location').value;
+
+    fetch(`http://localhost:3000/weather?location=${location}`).then(response => {
+        response.json().then(data => {
+            removeLoader();
+            writeResult(JSON.stringify(data));
+        });
+    });
+}
+
+function showLoader() {
+    resultElement.innerHTML = '';
+    const loader = document.createElement('p');
+    loader.id = 'loader';
+    loader.innerText = 'Loading...';
+
+    resultContainer.append(loader);
+}
+
+function removeLoader() {
+    const loader = document.getElementById('loader');
+    resultContainer.removeChild(loader);
+}
+
+function writeResult(result) {
+    const p = document.createElement('p');
+    p.innerText = result;
+
+    resultElement.innerHTML = '';
+
+    resultElement.append(p);
+}
---
+++ src/app.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
+const hbs = require('express-hbs');
+const {geocode} = require('../utils/geocode');
+const {openWeather} = require('../utils/openweather');
+
+const publicPath = path.join(__dirname, '..', 'public');
+const templates = path.join(__dirname, '..', 'templates');
+const viewsPath = path.join(templates, 'views');
+const partialsPath = path.join(templates, 'partials');
+
+const app = express();
+
+app.engine('hbs', hbs.express4({
+    partialsPath: partialsPath
+}))
+
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+
+const navigations = fs.readFileSync(path.join(partialsPath, 'navigation.hbs'));
+const header = fs.readFileSync(path.join(partialsPath, 'header.hbs'));
+
+hbs.registerPartial('navigation', navigations.toString());
+hbs.registerPartial('header', header.toString());
+
+app.use(express.static(publicPath));
+
+app.get('', (req, res) => {
+    return res.render('');
+});
+
+app.get('/weather', (req, res) => {
+    const {location} = req.query;
+
+    if (!location) {
+        return res.send({
+            error: 422,
+            message: 'You must provide location'
+        })
+    }
+
+    geocode(location, (err, result) => {
+        if (err) {
+            return res.send(err);
+        }
+
+        const {longitude, latitude} = result;
+
+        openWeather(longitude, latitude, (err, result) => {
+            if (err) {
+                return res.send(err);
+            }
+
+            return res.send(result);
+        });
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About page'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help page'
+    });
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
